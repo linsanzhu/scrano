0b9c8ba5e5fedae5371c3f16f1bf797881026797
binz <2287225730@qq.com>
2019-04-12 00:43:43 +0800

    完善模板

--- scrano/template/project/pipelines.js
+++ scrano/template/project/pipelines.js
@@ -1,12 +1,25 @@
 class Pipeline {
+
+    /**
+     * 任务开启时调用, 用于前置处理, 如建立与数据库的连接
+     */
     static engineOpended() {
         //
     }
 
+    /**
+     * 任务结束时调用, 用于后置处理, 如关闭与数据库的连接
+     */
     static engineClosed() {
         //
     }
 
+    /** 
+     * 
+     * @param {Item} item 
+     * @param {Spider} spider 
+     * @throws {DropItem} 表示item被处理, 不用向后继续传递了
+     */
     static processItem(item, spider) {
         //
     }
