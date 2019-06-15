9423465d382ce0e20c0efd339e0832c3bdbb40d2
binz <2287225730@qq.com>
2019-06-15 20:59:36 +0800

    update

--- test/request.test.js
+++ test/request.test.js
@@ -1,13 +1,40 @@
 const { Request, } = require('../scrano/request')
 const exceptions = require('../scrano/exception')
 const fetch = require('node-fetch')
+const http = require("http")
+const url = require("url")
 
+http.createServer(function(req, res) {
+    const options = url.parse(req.url)
+    options.headers = req.headers
+
+    const proxyRequest = http.request(options, function(proxyResponse) {
+        proxyResponse.on('data', function(chunk) {
+            console.log('proxyResponse length:', chunk.length)
+            res.write(chunk, 'binary')
+        })
+        proxyResponse.on('end', function() {
+            res.end()
+        })
+
+        res.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+    }) 
+
+    req.on('data', function(chunk) {
+        proxyRequest.write(chunk, 'binary')
+    })
+
+    req.on('end', function() {
+        proxyRequest.end()
+    })
+
+}).listen(8080)
 
 test('set-proxy', () => {
     const request = new Request('http://www.baidu.com', () => {
         //
     })
-    request.setProxy({host: '112.80.41.85', port: 8888, })
+    request.setProxy({host: 'localhost', port: 8080, })
     return fetch(request.url, request.meta).then((response) => {
         expect(response.ok).toBeTruthy()
     })
