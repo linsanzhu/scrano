29da9c4ed5647b36ef1b4f061a3e6c5c58743445
朱彬01 <zhubin01@ESG.360ES.CN>
2019-07-24 16:12:33 +0800

    update cookie middleware

--- scrano/downloadmiddlewares/cookies.js
+++ scrano/downloadmiddlewares/cookies.js
@@ -1,6 +1,24 @@
+import { CookieJar, } from 'http/cookies'
+
 class CookiesMiddleware {
-    constructor() {
-        throw new Error('not implement')
+    static init(options) {
+        const instance = new this()
+        instance.cookieJar = new CookieJar()
+        return instance
+    }
+
+    processRequest({request, spider, }) {
+        if (request.meta.dont_merge_cookies) {
+            return
+        }
+        this.cookieJar.addCookieHeader(request)
+    }
+
+    processResponse({response, spider, }) {
+        if (response.request.meta.dont_merge_cookies) {
+            return
+        }
+        this.cookieJar.extractCookies(response, response.request)
     }
 }
 
