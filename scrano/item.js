b90c5372fdb7baf45b26d1cc8983263b2bfa2149
binz <2287225730@qq.com>
2019-07-26 18:13:33 +0800

    Update item.js

--- scrano/item.js
+++ scrano/item.js
@@ -2,6 +2,24 @@ class Item {
     toString() {
         return `<Item ${this.name}>`
     }
+    engineOpended() {
+        //
+    }
+    
+    engineClosed() {
+        //
+    }
+    
+    /** 处理由spider返回的item
+     * 若返回undefine/null, 或者Item实例, 则继续交由下一个pipeline处理
+     * 若返回其他对象,则通过engine交由调度器调度
+     * 若抛出DropItem异常,则不再继续交由其他pipeline处理
+     * @param {Item} item 
+     * @param {Spider} spider 
+     */
+    processItem(item, spider) {
+        //
+    }
 }
 
 module.exports = {
